bc49e61d88efb685fcb5fa3e812743a0d233653e
Animesh Jain <anijain.0@gmail.com>
2018-01-07 20:52:51 +0530

    Initial commit

---
+++ app/src/main/java/com/animeshjain/assignment192/network/OnWebServiceResult.java
@@ -0,0 +1,8 @@
+package com.animeshjain.assignment192.network;
+
+
+import com.animeshjain.assignment192.utils.CommonUtilities;
+
+public interface OnWebServiceResult {
+    public void getWebResponse(String result, CommonUtilities.SERVICE_TYPE type);
+}
